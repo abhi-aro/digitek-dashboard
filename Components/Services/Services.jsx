b78b56d8da04da70d9013be5bce63ab5079c8b95
Abhishek <abhiaro27@gmail.com>
2023-08-30 23:49:46 +0530

    Temporary Payment Modal for services

--- Components/Services/Services.jsx
+++ Components/Services/Services.jsx
@@ -1,8 +1,12 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import Styles from "../../Styles/Services/Services.module.css";
 import { v4 as uuidv4 } from "uuid";
 
 const Services = () => {
+  const [paymentModal, setPaymentModal] = useState(false);
+
   const servicesData = [
     "SEO",
     "SEM",
@@ -19,21 +23,124 @@ const Services = () => {
     "Application Development",
   ];
   return (
-    <div className={Styles.wrapper}>
-      <div className={Styles.header}>Services</div>
-      <div className={Styles.container}>
-        {servicesData.map((item, ind) => {
-          return <ServiceItem key={uuidv4()} data={item} />;
-        })}
+    <>
+      <PaymentModal
+        paymentModal={paymentModal}
+        setPaymentModal={setPaymentModal}
+      />
+      <div className={Styles.wrapper}>
+        <div className={Styles.header}>Services</div>
+        <div className={Styles.container}>
+          {servicesData.map((item, ind) => {
+            return (
+              <ServiceItem
+                key={uuidv4()}
+                data={item}
+                setPaymentModal={setPaymentModal}
+              />
+            );
+          })}
+        </div>
       </div>
+    </>
+  );
+};
+
+const ServiceItem = ({ data, setPaymentModal }) => {
+  const askForPayment = () => {
+    setPaymentModal(true);
+  };
+
+  return (
+    <div key={uuidv4()} className={Styles.item} onClick={askForPayment}>
+      {data}
     </div>
   );
 };
 
-const ServiceItem = ({ data }) => {
-  return (
-    <div key={uuidv4()} className={Styles.item}>
-      {data}
+const PaymentModal = ({ paymentModal, setPaymentModal }) => {
+  const [paid, setPaid] = useState(false);
+  const [paymentStatus, setPaymentStatus] = useState("");
+
+  const closeModal = () => {
+    setPaymentModal(false);
+  };
+
+  const paymentRedirect = (e) => {
+    e.stopPropagation();
+    setPaid(true);
+    setPaymentStatus("Payment Successful");
+    setTimeout(() => {
+      setPaymentStatus("Redirecting to {INVALID SITE}");
+    }, 2000);
+    setTimeout(() => {
+      setPaymentStatus("JK 😁");
+    }, 3000);
+    setTimeout(() => {
+      setPaymentStatus("Redirecting to UV desk");
+      window.location.href =
+        "http://service.digitekservice.com/helpdesk-project/public/en/member/login";
+    }, 4000);
+  };
+
+  return paymentModal == false ? (
+    <></>
+  ) : (
+    <div
+      style={{
+        position: "absolute",
+        top: "0px",
+        left: "0px",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "100dvh",
+        width: "100dvw",
+        background: "rgba(0,0,0, 0.5)",
+      }}
+      onClick={closeModal}
+    >
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          height: "100%",
+          maxHeight: "40vh",
+          width: "100%",
+          maxWidth: "40vw",
+          background: "#343540",
+          borderRadius: "12px",
+        }}
+        onClick={(e) => e.stopPropagation()}
+      >
+        {paid == true ? (
+          <>
+            <div
+              style={{
+                padding: "20px",
+                background: "rgba(75,181,67, 0.8)",
+                borderRadius: "12px",
+                cursor: "pointer",
+              }}
+            >
+              {paymentStatus}
+            </div>
+          </>
+        ) : (
+          <div
+            style={{
+              padding: "20px",
+              background: "rgba(0,0,0,.5)",
+              borderRadius: "12px",
+              cursor: "pointer",
+            }}
+            onClick={paymentRedirect}
+          >
+            Click me to pay
+          </div>
+        )}
+      </div>
     </div>
   );
 };
