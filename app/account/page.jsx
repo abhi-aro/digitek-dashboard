6c225c26054cabddedcabc77dd00954cfb8eb516
skbhoi2001 <soumyak.bhoi.2001@gmail.com>
2023-11-09 00:24:46 +0530

    Account added

--- app/account/page.jsx
+++ app/account/page.jsx
@@ -1,7 +1,52 @@
-import React from "react";
+"use client";
+
+import React, { useEffect, useState } from "react";
+import { useSession } from "next-auth/react";
+import { api_UpdateUser } from "../api/Authentication";
+import Styles from "../../Styles/Account/Account.module.css";
 
 const Account = () => {
-  return <div>Account</div>;
+  const { data: session } = useSession();
+
+  const [data, setData] = useState({});
+  console.debug(data);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    let response = await api_UpdateUser({
+      data: { email: session?.user?.email },
+    });
+    if (response?.data?.body?.stage == 0) {
+      redirect("/formData");
+    } else {
+      setData(response?.data?.body?.data);
+    }
+  };
+
+  return (
+    <div className={Styles.accMainContainer}>
+      <div className={Styles.detailsHead}>Details</div>
+      <div className={Styles.det1Main}>
+        <div className={Styles.det1Head1}>Company Name:</div>
+        <div className={Styles.det1Head2}>{data?.company_name}</div>
+      </div>
+      <div className={Styles.det1Main}>
+        <div className={Styles.det1Head1}>Registeration Code:</div>
+        <div className={Styles.det1Head2}>{data?.registration_code}</div>
+      </div>
+      <div className={Styles.det1Main}>
+        <div className={Styles.det1Head1}>Email:</div>
+        <div className={Styles.det1Head2}>{data?.email}</div>
+      </div>
+      <div className={Styles.det1Main}>
+        <div className={Styles.det1Head1}>Company Size:</div>
+        <div className={Styles.det1Head2}>{data?.company_size}</div>
+      </div>
+    </div>
+  );
 };
 
 export default Account;
---
+++ app/analytics/layout.jsx
@@ -0,0 +1,8 @@
+export const metadata = {
+  title: "DigiTek My Digital Assets",
+  description: "",
+};
+
+export default function AnalyticsLayout({ children }) {
+  return <>{children}</>;
+}
